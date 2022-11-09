df136c40050b7cc3cae27bb28581eadbe693f5a5
anirudh-92 <100350582+anirudh-92@users.noreply.github.com>
2022-11-09 18:58:10 +0000

    Add files via upload

---
+++ tf-ecr-ecs-main/terraform/vpc.tf
@@ -0,0 +1,23 @@
+/* data "aws_vpc" "vpc" {
+  id      = local.use_default_vpc ? null : local.vpc["id"]
+  default = local.use_default_vpc
+}
+
+data "aws_subnet_ids" "subnets" {
+  vpc_id = data.aws_vpc.vpc.id
+}
+
+data "aws_subnet" "subnets" {
+  for_each = data.aws_subnet_ids.subnets.ids
+  vpc_id   = data.aws_vpc.vpc.id
+  id       = each.value
+  # availability_zone = each.value
+} */
+
+data "aws_vpc" "default_vpc" {
+  default = true
+}
+
+data "aws_subnet_ids" "subnets" {
+  vpc_id = "${data.aws_vpc.default_vpc.id}"
+}
